7a683923a8c6876a9158da8d0e975715f57deed7
shuyalin <18075599795@163.com>
2018-12-19 15:48:56 +0800

    nothing

---
+++ helppage.h
@@ -0,0 +1,55 @@
+/********************************************************************************
+** Form generated from reading UI file 'helppage.ui'
+**
+** Created by: Qt User Interface Compiler version 5.2.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef HELPPAGE_H
+#define HELPPAGE_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Help
+{
+public:
+    QPushButton *pushButton_exit;
+
+    void setupUi(QWidget *Form)
+    {
+        if (Form->objectName().isEmpty())
+            Form->setObjectName(QStringLiteral("Form"));
+        Form->resize(400, 300);
+        pushButton_exit = new QPushButton(Form);
+        pushButton_exit->setObjectName(QStringLiteral("pushButton_exit"));
+        pushButton_exit->setGeometry(QRect(250, 240, 99, 27));
+
+        retranslateUi(Form);
+
+        QMetaObject::connectSlotsByName(Form);
+    } // setupUi
+
+    void retranslateUi(QWidget *Form)
+    {
+        Form->setWindowTitle(QApplication::translate("Form", "Form", 0));
+        pushButton_exit->setText(QApplication::translate("Form", "\351\200\200\345\207\272", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Help: public Ui_Help {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // HELPPAGE_H
